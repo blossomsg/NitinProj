69d6c79d975c5b25d97a532017ccf9be431c9840
Blossom Ghuntla <bghuntla123@gmail.com>
2018-11-17 16:56:13 +0530

    01 Message: function to create proj structure, docstring, if __name__ are done

---
+++ create_project_structure/createstructure.py
@@ -0,0 +1,16 @@
+import os
+
+
+def create_structure(path):
+    """ It will create a proj structure when provided path of a folder
+
+    Args:
+        path:
+
+    Returns:
+
+    """
+
+
+if __name__ == '__main__':
+    print "This is my main function"
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="NitinProj",
+    version="0.1dev",
+    packages=["createprojstructure", ],
+    license="MIT",
+    long_description=open("README.txt").read(),
+)
