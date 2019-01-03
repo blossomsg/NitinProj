0baa6e8ded20d5553ded217b43c1407a5ba5e873
blossomsg <38830385+blossomsg@users.noreply.github.com>
2019-01-03 21:30:55 +0530

    08 Message:MajorChanges updated mass_playblast_lay variables prefix with "animcog_mpb_..." applied css to the layout added buttons of vp(4) fixed vp buttons size fixed size of the toolbox for now according to pb

--- anim_conglomeration/anim_conglomeration_ui.py
+++ anim_conglomeration/anim_conglomeration_ui.py
@@ -7,9 +7,9 @@ from shiboken2 import wrapInstance
 
 import sys
 
-path_anim_tools = "D:\\All_Projs\\NitinProj\\anim_conglomeration\\"
+path_anim_tools = "E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\"
 if not path_anim_tools in sys.path:
-	sys.path.append(path_anim_tools)
+    sys.path.append(path_anim_tools)
 import mass_playblast_lay
 
 reload(mass_playblast_lay)
@@ -19,30 +19,30 @@ ptr_instance = wrapInstance(long(ptr), QtWidgets.QWidget)
 
 
 class AnimConglomerationUI(mass_playblast_lay.MassPlayblastLay):
-	def __init__(self):
-		super(AnimConglomerationUI, self).__init__()
-		# CAVEAT : Parent ui to the application through which the ui is being created
-		self.setParent(ptr_instance)
-		self.setWindowFlags(QtCore.Qt.Window)
+    def __init__(self):
+        super(AnimConglomerationUI, self).__init__()
+        # CAVEAT : Parent ui to the application through which the ui is being created
+        self.setParent(ptr_instance)
+        self.setWindowFlags(QtCore.Qt.Window)
 
-		# CAVEAT : Playblast Toolbox
-		self.animcog_toolbox = QtWidgets.QToolBox()
-		self.animcog_toolbox_comingsoon_column = QtWidgets.QWidget()
+        # CAVEAT : Playblast Toolbox
+        self.animcog_toolbox = QtWidgets.QToolBox()
+        self.animcog_toolbox_comingsoon_column = QtWidgets.QWidget()
 
-		# CAVEAT : Layout for the Toolbox widgets
-		self.animcog_grid_layout = QtWidgets.QGridLayout()
+        # CAVEAT : Layout for the Toolbox widgets
+        self.animcog_grid_layout = QtWidgets.QGridLayout()
 
-		# CAVEAT : Adding widget to MassLayouts/ Connections
-		self.animcog_toolbox.addItem(self.animcog_toolbox_palyblast_column, "Playblast")
-		self.animcog_toolbox.addItem(self.animcog_toolbox_comingsoon_column, "ComingSoon")
-		self.animcog_grid_layout.addWidget(self.animcog_toolbox)
-		self.setLayout(self.animcog_grid_layout)
+        # CAVEAT : Adding widget to MassLayouts/ Connections
+        self.animcog_toolbox.addItem(self.animcog_mpb_toolbox_palyblast_column, "Playblast")
+        self.animcog_toolbox.addItem(self.animcog_toolbox_comingsoon_column, "ComingSoon")
+        self.animcog_grid_layout.addWidget(self.animcog_toolbox)
+        self.setLayout(self.animcog_grid_layout)
 
-		self.setWindowTitle("animcogact")
-	# self.setFixedSize(300, 130)
+        self.setWindowTitle("animcog v1.0")
+        self.setFixedSize(250, 230)
 
 
 if __name__ == "__main__":
-	print "This is my main function"
-	window = AnimConglomerationUI()
-	window.show()
+    print "This is my animcog function"
+    window = AnimConglomerationUI()
+    window.show()
--- anim_conglomeration/mass_playblast_lay.py
+++ anim_conglomeration/mass_playblast_lay.py
@@ -3,41 +3,102 @@ from PySide2 import QtGui
 
 
 class MassPlayblastLay(QtWidgets.QWidget):
+	"""animcog_mpb is abbreviation for animconglomeration_massplayblast
+
+	"""
 	def __init__(self):
 		super(MassPlayblastLay, self).__init__()
 		# CAVEAT : Playblast Toolbox Layout
-		self.animcog_toolbox_palyblast_column = QtWidgets.QWidget()
+		self.animcog_mpb_toolbox_palyblast_column = QtWidgets.QWidget()
+		self.animcog_mpb_toolbox_palyblast_column.setStyleSheet("QWidget {background-color:#95c7db;}")
+
 
 		# CAVEAT : Default Vp settings
-		self.animcog_textured_qpushbutton = QtWidgets.QToolButton.setIcon(QtGui.QIcon("D:\\All_Projs\\NitinProj\\anim_conglomeration\\chess.png"))
+		self.animcog_mpb_ambocclusion_qpushbutton = QtWidgets.QPushButton()
+		self.animcog_mpb_ambocclusion_qpushbutton.setIcon(QtGui.QIcon("E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\icons\\question-mark.png"))
+		self.animcog_mpb_ambocclusion_qpushbutton.setFlat(True)
+		self.animcog_mpb_ambocclusion_qpushbutton.setCheckable(True)
+		self.animcog_mpb_ambocclusion_qpushbutton.setFixedSize(20, 20)
+		self.animcog_mpb_ambocclusion_qpushbutton.setStyleSheet("QPushButton:pressed{background:#054f6d; border:none} "
+															"QPushButton:checked{background:#054f6d; border:none} "
+															"QPushButton:hover:checked{image: url("
+															"E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\icons"
+															"\\question-mark.png); border:none} QPushButton:hover{"
+															"image: url(E:\\Proj_Codes\\NitinProj\\anim_conglomeration"
+															"\\icons\\question-mark.png); border:none}")
+		self.animcog_mpb_usealllights_qpushbutton = QtWidgets.QPushButton()
+		self.animcog_mpb_usealllights_qpushbutton.setIcon(QtGui.QIcon("E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\icons\\question-mark.png"))
+		self.animcog_mpb_usealllights_qpushbutton.setFlat(True)
+		self.animcog_mpb_usealllights_qpushbutton.setCheckable(True)
+		self.animcog_mpb_usealllights_qpushbutton.setFixedSize(20, 20)
+		self.animcog_mpb_usealllights_qpushbutton.setStyleSheet("QPushButton:pressed{background:#054f6d; border:none} "
+															"QPushButton:checked{background:#054f6d; border:none} "
+															"QPushButton:hover:checked{image: url("
+															"E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\icons"
+															"\\question-mark.png); border:none} QPushButton:hover{"
+															"image: url(E:\\Proj_Codes\\NitinProj\\anim_conglomeration"
+															"\\icons\\question-mark.png); border:none}")
+		self.animcog_mpb_shadows_qpushbutton = QtWidgets.QPushButton()
+		self.animcog_mpb_shadows_qpushbutton.setIcon(
+			QtGui.QIcon("E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\icons\\question-mark.png"))
+		self.animcog_mpb_shadows_qpushbutton.setFlat(True)
+		self.animcog_mpb_shadows_qpushbutton.setCheckable(True)
+		self.animcog_mpb_shadows_qpushbutton.setFixedSize(20, 20)
+		self.animcog_mpb_shadows_qpushbutton.setStyleSheet("QPushButton:pressed{background:#054f6d; border:none} "
+																"QPushButton:checked{background:#054f6d; border:none} "
+																"QPushButton:hover:checked{image: url("
+																"E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\icons"
+																"\\question-mark.png); border:none} QPushButton:hover{"
+																"image: url(E:\\Proj_Codes\\NitinProj\\anim_conglomeration"
+																"\\icons\\question-mark.png); border:none}")
+		self.animcog_mpb_antialiasing_qpushbutton = QtWidgets.QPushButton()
+		self.animcog_mpb_antialiasing_qpushbutton.setIcon(
+			QtGui.QIcon("E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\icons\\question-mark.png"))
+		self.animcog_mpb_antialiasing_qpushbutton.setFlat(True)
+		self.animcog_mpb_antialiasing_qpushbutton.setCheckable(True)
+		self.animcog_mpb_antialiasing_qpushbutton.setFixedSize(20, 20)
+		self.animcog_mpb_antialiasing_qpushbutton.setStyleSheet("QPushButton:pressed{background:#054f6d; border:none} "
+														   "QPushButton:checked{background:#054f6d; border:none} "
+														   "QPushButton:hover:checked{image: url("
+														   "E:\\Proj_Codes\\NitinProj\\anim_conglomeration\\icons"
+														   "\\question-mark.png); border:none} QPushButton:hover{"
+														   "image: url(E:\\Proj_Codes\\NitinProj\\anim_conglomeration"
+														   "\\icons\\question-mark.png); border:none}")
 
 		# CAVEAT : Browser
-		self.animcog_path_qlineedit = QtWidgets.QLineEdit()
-		self.animcog_browse_qpushbutton = QtWidgets.QPushButton("Browse")
+		self.animcog_mpb_path_qlineedit = QtWidgets.QLineEdit()
+		self.animcog_mpb_path_qlineedit.setStyleSheet("QLineEdit {background-color:#054f6d; color:#81d0ef; border: 1px solid #054f6d;}")
+		self.animcog_mpb_browse_qpushbutton = QtWidgets.QPushButton("Browse")
+		self.animcog_mpb_browse_qpushbutton.setStyleSheet("QPushButton {background-color:#054f6d; color:#81d0ef; }")
 
 		# CAVEAT : Viewport Mode List
-		self.animcog_vp_combobox = QtWidgets.QComboBox()
-		self.animcog_vp_2_combobox = self.animcog_vp_combobox.addItem(
+		self.animcog_mpb_vp_combobox = QtWidgets.QComboBox()
+		self.animcog_mpb_vp_combobox.setStyleSheet("QComboBox {background-color:#054f6d; color:#81d0ef; background:#054f6d} QComboBox QAbstractItemView {background-color:#054f6d; color:#81d0ef; background:#054f6d}")
+		self.animcog_mpb_vp_2_combobox = self.animcog_mpb_vp_combobox.addItem(
 			"Viewport 2.0")
-		self.animcog_ldvp_combobox = self.animcog_vp_combobox.addItem(
+		self.animcog_mpb_ldvp_combobox = self.animcog_mpb_vp_combobox.addItem(
 			"Legacy Default Viewport")
-		self.animcog_lhqvp_combobox = self.animcog_vp_combobox.addItem(
+		self.animcog_mpb_lhqvp_combobox = self.animcog_mpb_vp_combobox.addItem(
 			"Legacy High Quality Viewport")
-		self.animcog_playblast_qpushbutton = QtWidgets.QPushButton("Playblast")
+		self.animcog_mpb_playblast_qpushbutton = QtWidgets.QPushButton("Playblast")
+		self.animcog_mpb_playblast_qpushbutton.setStyleSheet("QPushButton {background-color:#054f6d; color:#81d0ef; }")
 
 		# CAVEAT : Layout for the multiple widgets
-		self.animcog_parenthorizontal_vertical_layout = QtWidgets.QVBoxLayout(self.animcog_toolbox_palyblast_column)
-		self.animcog_browser_horizontal_layout = QtWidgets.QHBoxLayout()
-		self.animcog_switches_buttons_horizontal_layout = QtWidgets.QHBoxLayout()
+		self.animcog_mpb_parent_horizontal_vertical_layout = QtWidgets.QVBoxLayout(self.animcog_mpb_toolbox_palyblast_column)
+		self.animcog_mpb_browser_horizontal_layout = QtWidgets.QHBoxLayout()
+		self.animcog_mpb_vp_buttons = QtWidgets.QHBoxLayout()
 
 		# CAVEAT : Adding widget to Layouts/Connections
-		self.animcog_browser_horizontal_layout.addWidget(self.animcog_path_qlineedit)
-		self.animcog_browser_horizontal_layout.addWidget(self.animcog_browse_qpushbutton)
-		self.animcog_switches_buttons_horizontal_layout.addWidget(self.animcog_textured_qpushbutton)
-		self.animcog_parenthorizontal_vertical_layout.addLayout(self.animcog_browser_horizontal_layout)
-		self.animcog_parenthorizontal_vertical_layout.addLayout(self.animcog_switches_buttons_horizontal_layout)
-		self.animcog_parenthorizontal_vertical_layout.addWidget(self.animcog_vp_combobox)
-		self.animcog_parenthorizontal_vertical_layout.addWidget(self.animcog_playblast_qpushbutton)
+		self.animcog_mpb_browser_horizontal_layout.addWidget(self.animcog_mpb_path_qlineedit)
+		self.animcog_mpb_browser_horizontal_layout.addWidget(self.animcog_mpb_browse_qpushbutton)
+		self.animcog_mpb_vp_buttons.addWidget(self.animcog_mpb_ambocclusion_qpushbutton)
+		self.animcog_mpb_vp_buttons.addWidget(self.animcog_mpb_usealllights_qpushbutton)
+		self.animcog_mpb_vp_buttons.addWidget(self.animcog_mpb_shadows_qpushbutton)
+		self.animcog_mpb_vp_buttons.addWidget(self.animcog_mpb_antialiasing_qpushbutton)
+		self.animcog_mpb_parent_horizontal_vertical_layout.addLayout(self.animcog_mpb_browser_horizontal_layout)
+		self.animcog_mpb_parent_horizontal_vertical_layout.addLayout(self.animcog_mpb_vp_buttons)
+		self.animcog_mpb_parent_horizontal_vertical_layout.addWidget(self.animcog_mpb_vp_combobox)
+		self.animcog_mpb_parent_horizontal_vertical_layout.addWidget(self.animcog_mpb_playblast_qpushbutton)
 
 
 
