4d12117aa9023a9ce08057539999a356a79821af
blossomsg <38830385+blossomsg@users.noreply.github.com>
2019-01-07 23:10:17 +0530

    11 Message:MajorChanges toggle buttons fixed and working realtime combobox list working realtime new functions added of the vp_mode and vp_buttons

--- anim_conglomeration/mass_playblast_lay_func.py
+++ anim_conglomeration/mass_playblast_lay_func.py
@@ -17,24 +17,58 @@ class MassPlayblastLayFunc(anim_conglomeration_ui.AnimConglomerationUI):
         super(MassPlayblastLayFunc, self).__init__()
 
         self.animcog_mpb_browse_qpushbutton.clicked.connect(self.animcog_mpb_browse_button_func)
-        self.animcog_mpb_usealllights_qpushbutton.pressed.connect(self.animcog_mpb_usealllights_toggle_button_on_func)
-        # self.animcog_mpb_usealllights_qpushbutton.connect(self.animcog_mpb_usealllights_toggle_button_off_func)
+        self.animcog_mpb_usealllights_qpushbutton.clicked.connect(self.animcog_mpb_usealllights_toggle_button_func)
+        self.animcog_mpb_shadows_qpushbutton.clicked.connect(self.animcog_mpb_shadows_toggle_button_func)
+        self.animcog_mpb_ambocclusion_qpushbutton.clicked.connect(self.animcog_mpb_ambocclusion_toggle_button_func)
+        self.animcog_mpb_antialiasing_qpushbutton.clicked.connect(self.animcog_mpb_antialiasing_toggle_button_func)
+        self.animcog_mpb_vp_combobox.activated.connect(self.anim_mpb_vp_mode_func)
+
+        cmds.modelEditor("modelPanel4", edit=True, rendererName="vp2Renderer")
+
+        cmds.modelEditor("modelPanel4", edit=True, displayLights="none")
+        cmds.modelEditor("modelPanel4", edit=True, shadows=False)
+        cmds.setAttr("hardwareRenderingGlobals.ssaoEnable", 0)
+        cmds.setAttr("hardwareRenderingGlobals.multiSampleEnable", 0)
 
     def animcog_mpb_browse_button_func(self):
         self.browse_path = cmds.fileDialog2(dialogStyle=2)
         print "Playblast Saving Path --> %s" %self.browse_path[0]
         self.animcog_mpb_path_qlineedit.setText(self.browse_path[0])
 
-    def animcog_mpb_usealllights_toggle_button_on_func(self):
-        cmds.modelEditor("modelPanel4", edit=True, displayLights="all")
+    def animcog_mpb_usealllights_toggle_button_func(self):
+        if self.animcog_mpb_usealllights_qpushbutton.isChecked():
+            cmds.modelEditor("modelPanel4", edit=True, displayLights="all")
+        else:
+            cmds.modelEditor("modelPanel4", edit=True, displayLights="none")
 
-    def animcog_mpb_usealllights_toggle_button_off_func(self):
-        cmds.modelEditor("modelPanel4", edit=False, displayLights="all")
+    def animcog_mpb_shadows_toggle_button_func(self):
+        if self.animcog_mpb_shadows_qpushbutton.isChecked():
+            cmds.modelEditor("modelPanel4", edit=True, shadows=True)
+        else:
+            cmds.modelEditor("modelPanel4", edit=True, shadows=False)
 
-        #
-        # cmds.modelEditor("modelPanel4", edit=True, shadows=True)
-        # cmds.setAttr("hardwareRenderingGlobals.ssaoEnable", 1)
-        # cmds.setAttr("hardwareRenderingGlobals.multiSampleEnable", 1)
+    def animcog_mpb_ambocclusion_toggle_button_func(self):
+        if self.animcog_mpb_ambocclusion_qpushbutton.isChecked():
+            cmds.setAttr("hardwareRenderingGlobals.ssaoEnable", 1)
+        else:
+            cmds.setAttr("hardwareRenderingGlobals.ssaoEnable", 0)
+
+    def animcog_mpb_antialiasing_toggle_button_func(self):
+        if self.animcog_mpb_antialiasing_qpushbutton.isChecked():
+            cmds.setAttr("hardwareRenderingGlobals.multiSampleEnable", 1)
+        else:
+            cmds.setAttr("hardwareRenderingGlobals.multiSampleEnable", 0)
+
+    def anim_mpb_vp_mode_func(self):
+        if self.animcog_mpb_vp_combobox.currentText() == "Viewport 2.0":
+            print self.animcog_mpb_vp_combobox.currentText()
+            cmds.modelEditor("modelPanel4", edit=True, rendererName="vp2Renderer")
+        elif self.animcog_mpb_vp_combobox.currentText() == "Legacy Default Viewport":
+            print self.animcog_mpb_vp_combobox.currentText()
+            cmds.modelEditor("modelPanel4", edit=True, rendererName="base_OpenGL_Renderer")
+        elif self.animcog_mpb_vp_combobox.currentText() == "Legacy High Quality Viewport":
+            print self.animcog_mpb_vp_combobox.currentText()
+            cmds.modelEditor("modelPanel4", edit=True, rendererName="hwRender_OpenGL_Renderer")
 
 #
 # if __name__ == "__main__":
@@ -51,3 +85,18 @@ class MassPlayblastLayFunc(anim_conglomeration_ui.AnimConglomerationUI):
 # reload(mass_playblast_lay_func)
 # anicog_win = mass_playblast_lay_func.MassPlayblastLayFunc()
 # anicog_win.show()
+
+
+# import maya.cmds as cmds
+# cmds.fileDialog2(dialogStyle=2)
+# cmds.modelEditor( 'modelPanel4', q=True, rls=True )
+# cmds.getPanel (wf=1)
+# cmds.modelEditor("modelPanel4", edit=True, displayLights="all")
+# cmds.modelEditor("modelPanel4", edit=True, shadows=True)
+# cmds.setAttr ("hardwareRenderingGlobals.ssaoEnable", 1)
+# cmds.setAttr ("hardwareRenderingGlobals.multiSampleEnable", 1)
+# cmds.modelEditor("modelPanel4", query=True, rendererList=True)
+# cmds.modelEditor("modelPanel4", edit=True, rendererName="vp2Renderer")
+# cmds.modelEditor("modelPanel4", edit=True, rendererName="hwRender_OpenGL_Renderer")
+# cmds.modelEditor("modelPanel4", edit=True, rendererName="base_OpenGL_Renderer")
+# # Result: [u'vp2Renderer',u'base_OpenGL_Renderer',u'hwRender_OpenGL_Renderer',u'stub_Renderer'] #
